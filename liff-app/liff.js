a0365597399fce61d23338dd34715cd4d249aafc
hsiachu <46018653+hsiachu@users.noreply.github.com>
2019-01-23 17:27:57 +0800

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -170,7 +170,7 @@ function liffConnectToDevice(device) {
     device.gatt.connect().then(() => {
         document.getElementById("device-name").innerText = device.name;
         document.getElementById("device-id").innerText = device.id;
-
+document.getElementById("device-name").innerText = "00";
         // Show status connected
         uiToggleDeviceConnected(true);
 
@@ -211,7 +211,7 @@ function liffConnectToDevice(device) {
 }
 
 function liffGetUserService(service) {
-    document.getElementById("device-name").innerText = 11;
+    document.getElementById("device-name").innerText = "11";
     // Button pressed state
     //service.getCharacteristic(BTN_CHARACTERISTIC_UUID).then(characteristic => {
         //liffGetButtonStateCharacteristic(characteristic);
@@ -221,7 +221,7 @@ function liffGetUserService(service) {
 
     // Toggle LED
     service.getCharacteristic(LED_CHARACTERISTIC_UUID).then(characteristic => {
-        document.getElementById("device-name").innerText=22;
+        document.getElementById("device-name").innerText="22";
         window.ledCharacteristic = characteristic;
 
         // Switch off by default
@@ -268,7 +268,7 @@ function liffGetButtonStateCharacteristic(characteristic) {
 function liffToggleDeviceLedState(state) {
     // on: 0x01
     // off: 0x00
-    document.getElementById("device-name").innerText=33;
+    document.getElementById("device-name").innerText="33";
     window.ledCharacteristic.writeValue(
         state ? new Uint8Array([0xAA023009]) : new Uint8Array([0xAA,0x02,0x30,0x09])
     ).catch(error => {
